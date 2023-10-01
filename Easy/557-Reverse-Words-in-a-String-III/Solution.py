3128b510d53f882417b418762f1d58a24c474463
Noor khalel <108550197+Noorkhalel@users.noreply.github.com>
2023-10-02 02:05:50 +0300

    4 day

---
+++ Easy/557-Reverse-Words-in-a-String-III/Solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def reverseWords(self, s):
+
+        a = []
+        f=""
+        s = s +" "
+        for x in s:
+            f=f+x
+            if x ==" ":
+               a.append(f)
+               f =""
+        
+        txt =""
+        # print(a)
+        for x in range(len(a)-1,-1,-1):
+            txt += a[x] 
+        print(txt)
+        txt = txt[::-1]
+        txt = txt[1:]
+
+        return txt
+    
+
+solution = Solution()
+s = "Let's take LeetCode contest"
+result = solution.reverseWords(s)
+print(result)
