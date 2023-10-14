2c86a6da62de06569fbad22973fc4f989847de1c
Noor khalel <108550197+Noorkhalel@users.noreply.github.com>
2023-10-14 19:52:57 +0300

    push

---
+++ Medium/739-Daily-Temperatures/Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res        
