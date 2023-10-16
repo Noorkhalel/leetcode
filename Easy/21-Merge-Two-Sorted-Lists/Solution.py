23a88bf29232a0c456f3b3dbdd037c8638b55a43
Noor khalel <108550197+Noorkhalel@users.noreply.github.com>
2023-10-16 16:25:22 +0300

    push

---
+++ Easy/21-Merge-Two-Sorted-Lists/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+        lil.next = self.mergeTwoLists(lil.next, big)
+        return lil
