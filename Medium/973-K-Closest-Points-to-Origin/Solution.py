df2f475952515564fc1b36099efcd14c300ebb25
Noor khalel <108550197+Noorkhalel@users.noreply.github.com>
2023-11-02 19:04:51 +0200

    day 28

---
+++ Medium/973-K-Closest-Points-to-Origin/Solution.py
@@ -0,0 +1,17 @@
+import heapq
+
+
+class Solution(object):
+    def kClosest(self, points, k):
+        minHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append((dist, x, y))
+        
+        heapq.heapify(minHeap)
+        res = []
+        for _ in range(k):
+            _, x, y = heapq.heappop(minHeap)
+            res.append((x, y))
+        return res
+        
