c093a808b7e98d107726b94b8afd38b94de796cb
Noorkhalel <noorkhslil101@gmail.com>
2023-11-04 21:04:51 +0100

    day30

---
+++ Easy/70-Climbing-Stairs/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 3:
+            return n
+        n1, n2 = 2, 3
+
+        for i in range(4, n + 1):
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+        return n2
+        
