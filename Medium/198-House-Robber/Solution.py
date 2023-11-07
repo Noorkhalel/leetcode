fb12bcf2370222d93cbb693be87375cae4e70bbc
Noorkhalel <noorkhslil101@gmail.com>
2023-11-07 17:50:12 +0100

    day 32

---
+++ Medium/198-House-Robber/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rob(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
